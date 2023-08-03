ca992416f0380b078c7e864f048515f1949dc4ff
Umar-Waseem <umar.waseem@gmail.com>
2023-08-03 13:15:58 +0500

    write and query influx db

--- server/controllers/influx.js
+++ server/controllers/influx.js
@@ -1,24 +1,57 @@
 import { Point } from "@influxdata/influxdb-client";
-import { writeClient, queryApi } from "./influx_db_config.js";
+import { InfluxDB } from "@influxdata/influxdb-client";
+import dotenv from 'dotenv';
 
 class InfluxController {
 
+    constructor() {
+        dotenv.config();
+
+        const TOKEN = process.env.INFLUX_DB_TOKEN;
+        const URL = process.env.INFLUX_URL;
+        const ORG = process.env.INFLUX_ORG;
+        const BUCKET = process.env.INFLUX_BUCKET;
+
+        const CLIENT = new InfluxDB({ url: URL, token: TOKEN });
+
+        this.writeClient = CLIENT.getWriteApi(ORG, BUCKET);
+
+        this.queryClient = CLIENT.getQueryApi(ORG);
+    }
+
     // write point
-    static async writePoint(measurement, tags, fields) {
-        const point = new Point(measurement)
-            .tag(tags)
-            .floatField(fields)
-        writeClient.writePoint(point);
+    async writePoint(measurement, tags, fields) {
+        try {
+            const point = new Point(measurement)
+                .tag(tags)
+                .floatField("value", fields);
+            this.writeClient.writePoint(point);
+            // this.writeClient.flush();
+            // this.writeClient.close();
+            console.log(`Point ${point.toLineProtocol()} written successfully!`);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // query data
-    static async queryData(query) {
-        const queryResult = queryApi.queryRows(query);
-        const points = [];
-        for await (const row of queryResult) {
-            points.push(row);
+    async queryData(query) {
+        try {
+            const results = []
+            this.queryClient.queryRaw(query).then((result) => {
+                result.split("\r\n").forEach((line) => {
+                    if (line.startsWith("#")) {
+                        console.log(`HEADERS: ${line}`);
+                    } else {
+                        console.log(`RESULT: ${line}`);
+                        results.push(line);
+                    }
+                })
+            })
+            return results;
+        } catch (error) {
+            console.log(error);
         }
-        return points;
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ import users from './controllers/users_list.js';
 
 import checkAuthMiddleware from './middlewares/check_auth.js';
 
+import InfluxController from './controllers/influx.js';
+
 // pick port from env else default 3000 
 const PORT = process.env.PORT || 3000;
 const HOST = "http://localhost"
@@ -30,6 +32,18 @@ app.get('/', (req, res) => {
 app.get("/allUsers", checkAuthMiddleware, (req, res) => {
     const allUsers = users;
     res.json(allUsers);
+})
+
+app.get("/influx", async (req, res) => {
+    try {
+        const controller = new InfluxController();
+        const number = Math.random() * 100;
+        await controller.writePoint("measurement", { location: "kathmandu" }, number);
+        const result = await controller.queryData(`from(bucket: "bucket") |> range(start: 0)`);
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 app.listen(PORT, () => {
